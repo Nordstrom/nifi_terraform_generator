6ce79b34e9f060b86de0d32e1164a1edba007dd0
Kailas Janardhanan <kailas.janardhanan2@nordstrom.com>
2018-09-07 18:32:08 +0530

    Removing copyright information

--- src/main/java/com/nordstrom/mlsort/TFGeneratorHelper.java
+++ src/main/java/com/nordstrom/mlsort/TFGeneratorHelper.java
@@ -1,10 +1,3 @@
-/**
- * Copyright(c) Nordstrom,Inc. All Rights reserved. This software is the confidential and
- * proprietary information of Nordstrom, Inc. ("Confidential Information"). You shall not disclose
- * such Confidential Information and shall use it only in accordance with the terms of the license
- * agreement you entered into with Nordstrom, Inc.
- */
-
 package com.nordstrom.mlsort;
 
 import static com.nordstrom.mlsort.TFGeneratorHelperUtil.generateMainAndVariablesTF;
